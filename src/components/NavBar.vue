7efe9fb066ba38eb961033f18fb6fa3a69db5a62
Le Khoa <lekhoa011159@gmail.com>
2023-07-15 21:49:55 +0700

    Initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header class="py-4 px-24 max-w-7xl mx-auto">
+    <div class="logo">
+      <h1>Cocktails</h1>
+    </div>
+
+    <nav class="links">
+      <RouterLink to="/">Meals</RouterLink>
+      <RouterLink to="/">Meals</RouterLink>
+    </nav>
+  </header>
+</template>
